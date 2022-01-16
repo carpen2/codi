cd63e16935a83a6598ea3a406cac99919c4d254e
carpen2 <96364083+carpen2@users.noreply.github.com>
2022-01-17 07:30:22 +0900

    Update test12.py

--- test12.py
+++ test12.py
@@ -9,43 +9,43 @@ secret = "mqsj8p9QYwd3A1Iich0KctXiz4ZJQrhDPxxA39DF"
 #변동성 돌파 전략으로 매수 목표가 조회
 def get_target_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     target_price = df.iloc[1]['open'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.5
     return target_price
 #시작가
 def get_open_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     open_price = df.iloc[1]['open']
     return open_price
 #종가
 def get_close_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     close_price = df.iloc[1]['close']
     return close_price
 #최저가
 def get_low_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     low_price = df.iloc[1]['low']
     return low_price
 #전날시가
 def get_open1_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     high_price = df.iloc[0]['open']
     return high_price
 #전날종가
 def get_close1_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     close_price = df.iloc[0]['close']
     return close_price
 #시작 시간 조회
 def get_start_time(ticker):
     time.sleep(0.1)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     start_time = df.index[1]
     return start_time
    
@@ -70,7 +70,7 @@ def get_avg_buy_price(ticker):
 def get_moving_average(window, ticker):
     try:
         time.sleep(0.05)
-        df = pyupbit.get_ohlcv(ticker, interval="minute60")
+        df = pyupbit.get_ohlcv(ticker, interval="minute240")
         ma = df['close'].rolling(window=window).mean()
         return ma[-1]
     except Exception as e:
@@ -79,7 +79,7 @@ def get_moving_average(window, ticker):
 def get_moving1_average(window, ticker):
     try:
         time.sleep(0.05)
-        df = pyupbit.get_ohlcv(ticker, interval="minute60")
+        df = pyupbit.get_ohlcv(ticker, interval="minute240")
         ma = df['close'].rolling(window=window).mean()
         return ma[-2]
     except Exception as e:
@@ -88,7 +88,7 @@ def get_moving1_average(window, ticker):
 #최고가 조회
 def get_high_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
     high_price = df.iloc[0]['high']
     return high_price
 
@@ -103,27 +103,28 @@ def main():
     while True:
         try:
             for ticker in ori_tickers:
-                print("매수시간입니다")           
                 now = datetime.datetime.now()
+                print(now)
                 start_time = get_start_time("KRW-BTC")
-                end_time = start_time + datetime.timedelta(seconds=3600)                 
+                end_time = start_time + datetime.timedelta(seconds=14400)
                 target_p = get_target_price(ticker)
                 open_p = get_open_price(ticker)
+                open1_p = get_open1_price(ticker)
+                close1_p = get_close1_price(ticker)
                 current_p = get_current_price(ticker)
                 ma5 = get_moving_average(5, ticker)
                 ma10 = get_moving_average(10, ticker)
                 ma20 = get_moving_average(20, ticker)
                 man5 = get_moving1_average(5, ticker)
                 man10 = get_moving1_average(10, ticker)
+                man20 = get_moving1_average(20, ticker)
                 krw = get_balance("KRW")
                 bct_balances = upbit.get_balance(ticker)
-                if start_time + datetime.timedelta(seconds=3300) < now < end_time:
-                    if ma20 < ma10 < ma5 and open_p < current_p and man5 < ma5 and man10 < ma10 and 300 < current_p:
+                if start_time < now < start_time + datetime.timedelta(seconds=300):
+                    if man20 < man10 < man5 < current_p and open1_p < close1_p and man5 < ma5 and man10 < ma10 and 300 < current_p:
                     #target_p < current_p and ma10 < ma5 and open_p * 1.02 < current_p
-                        print(ticker)
-                        if krw > 10500 and bct_balances == 0:
+                        if krw > 10500 and bct_balances < (3000/current_p):
                             upbit.buy_market_order(ticker, 10000)
-                            print(ticker, "매수")
                         if krw < 10500:
                             print("금액 부족해서 매수 못해요")
                 else:
@@ -135,19 +136,20 @@ def main():
 def submain():
     while True:
         try:
-            print("손절시간입니다") 
             now = datetime.datetime.now()
+            print(now) 
             start_time = get_start_time("KRW-BTC")
-            end_time = start_time + datetime.timedelta(seconds=3600) 
+            end_time = start_time + datetime.timedelta(seconds=14400) 
             for ticker in ori_tickers:
                 bct_balances = upbit.get_balance(ticker)
                 buy_p = get_avg_buy_price(ticker)
                 current_p = get_current_price(ticker)
-                if start_time < now < start_time + datetime.timedelta(seconds=3000):
-                    if bct_balances > 0:
-                        if current_p < buy_p * 0.97:
-                            upbit.sell_market_order(ticker, bct_balances * 0.995)
-                            print(ticker, "부분손절")
+                if start_time + datetime.timedelta(seconds=300) < now < start_time + datetime.timedelta(seconds=14100):
+                    if bct_balances > (3000/current_p):
+                        if current_p < buy_p * 0.985:
+                            upbit.sell_market_order(ticker, bct_balances * 0.999)
+                    if bct_balances < (3000/current_p):
+                        pass
                 else:
                     submain2()
                     break
@@ -157,22 +159,16 @@ def submain():
 def submain2():
     while True:
         try:
-            print("매도시간입니다")       
             now = datetime.datetime.now()
+            print(now) 
             start_time = get_start_time("KRW-BTC")
-            end_time = start_time + datetime.timedelta(seconds=3600)
+            end_time = start_time + datetime.timedelta(seconds=14400)
             for ticker in ori_tickers:
                 bct_balances = upbit.get_balance(ticker)
                 current_p = get_current_price(ticker)
-                if start_time + datetime.timedelta(seconds=3000) < now < start_time + datetime.timedelta(seconds=3300):      
+                if start_time + datetime.timedelta(seconds=14100) < now < end_time:
                     if bct_balances > (5000/current_p) and bct_balances > 0:
-                        upbit.sell_market_order(ticker, bct_balances)
-                        print(ticker, "완전매도")
-                    if 0 < bct_balances < (5000/current_p):
-                        upbit.buy_market_order(ticker, 5100)
-                        print(ticker, "5천매수")
-                    if bct_balances == 0:
-                        pass
+                        upbit.sell_market_order(ticker, bct_balances * 0.999)
                 else:
                     main()
                     break
@@ -181,9 +177,8 @@ def submain2():
 
 while True:
     now = datetime.datetime.now() 
-    if now.hour == 23 and now.minute == 20 and 1 <=now.second <= 10:                
+    if now.hour == 8 and now.minute == 0 and 1 <=now.second <= 10:
         op_mode = True
-        print("시작")
     if op_mode == True:
         main()
         time.sleep(1)
