8703f1e02f80bd154033c09f00f6ce91877b1889
carpen2 <96364083+carpen2@users.noreply.github.com>
2022-04-07 21:08:45 +0900

    Update test14.py

--- test14.py
+++ test14.py
@@ -9,43 +9,43 @@ secret = "mqsj8p9QYwd3A1Iich0KctXiz4ZJQrhDPxxA39DF"
 #변동성 돌파 전략으로 매수 목표가 조회
 def get_target_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
-    target_price = df.iloc[1]['low'] + (df.iloc[0]['high'] - df.iloc[0]['low'])*0.54
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
+    target_price = df.iloc[1]['low'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.54
     return target_price
 #시작가
 def get_open_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     open_price = df.iloc[1]['open']
     return open_price
 #종가
 def get_close_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     close_price = df.iloc[1]['close']
     return close_price
 #최저가
 def get_low_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     low_price = df.iloc[1]['low']
     return low_price
 #전날시가
 def get_open1_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     high_price = df.iloc[0]['open']
     return high_price
 #전날종가
 def get_close1_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     close_price = df.iloc[0]['close']
     return close_price
 #시작 시간 조회
 def get_start_time(ticker):
     time.sleep(0.1)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     start_time = df.index[1]
     return start_time  
 #잔고 조회
@@ -69,7 +69,7 @@ def get_avg_buy_price(ticker):
 def get_moving_average(window, ticker):
     try:
         time.sleep(0.05)
-        df = pyupbit.get_ohlcv(ticker, interval="minute240")
+        df = pyupbit.get_ohlcv(ticker, interval="minute60")
         ma = df['close'].rolling(window=window).mean()
         return ma[-1]
     except Exception as e:
@@ -78,7 +78,7 @@ def get_moving_average(window, ticker):
 def get_moving1_average(window, ticker):
     try:
         time.sleep(0.05)
-        df = pyupbit.get_ohlcv(ticker, interval="minute240")
+        df = pyupbit.get_ohlcv(ticker, interval="minute60")
         ma = df['close'].rolling(window=window).mean()
         return ma[-2]
     except Exception as e:
@@ -86,7 +86,7 @@ def get_moving1_average(window, ticker):
 #최고가 조회
 def get_high_price(ticker):
     time.sleep(0.05)
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
     high_price = df.iloc[0]['high']
     return high_price
 # 로그인
@@ -102,7 +102,7 @@ async def main():
             for ticker in ori_tickers:
                 now = datetime.datetime.now()
                 start_time = get_start_time("KRW-BTC")
-                end_time = start_time + datetime.timedelta(seconds=14400)                 
+                end_time = start_time + datetime.timedelta(seconds=3600)                 
                 target_p = get_target_price(ticker)
                 current_p = get_current_price(ticker)
                 ma5 = get_moving_average(5, ticker)
@@ -110,10 +110,10 @@ async def main():
                 man5 = get_moving1_average(5, ticker)
                 krw = get_balance("KRW")
                 bct_balances = upbit.get_balance(ticker)
-                if start_time < now < end_time - datetime.timedelta(seconds=600) :
-                    if target_p < current_p and man5 < ma5 and ma10 < ma5:
-                        if krw > 1900000 and bct_balances == 0 and 200 < current_p:
-                            upbit.buy_market_order(ticker, 10000)
+                if start_time < now < end_time - datetime.timedelta(seconds=500):
+                    if target_p < current_p and ma10 < man5 < ma5:
+                        if krw > 1700000 and bct_balances == 0 and 300 < current_p:
+                            upbit.buy_market_order(ticker, 100000)
                         await asyncio.sleep(2)
                 else:
                     if 0 < bct_balances:
@@ -133,8 +133,8 @@ async def submain():
                 buy_p = get_avg_buy_price(ticker)
                 bct_balances = upbit.get_balance(ticker)
                 if 0 < bct_balances:
-                    if (9000/current_p) < bct_balances < (15000/current_p):
-                        if current_p > buy_p*1.025:
+                    if (80000/current_p) < bct_balances < (120000/current_p):
+                        if current_p > buy_p*1.1:
                            upbit.sell_market_order(ticker, bct_balances*0.999)
                 await asyncio.sleep(0.5)
         except Exception as e:
